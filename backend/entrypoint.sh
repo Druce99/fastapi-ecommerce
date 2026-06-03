7f53d32765401e3be4609fa92b59e702abcc8ed4
Druce99 <solaa07@gmail.com>
2026-06-03 18:02:39 +0300

    fix: trust forwarded headers from nginx proxy

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source .venv/bin/activate
 alembic upgrade head
-exec gunicorn src.main:app -w 4 -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000
+exec gunicorn src.main:app -w 4 -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000 --forwarded-allow-ips='*'
